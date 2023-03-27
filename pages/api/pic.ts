52ef026780d3495f8b5db27131ebb246353be209
StephenHogsten <StephenHogsten@users.noreply.github.com>
2023-03-27 12:20:06 -0700

    allow passing external url

--- pages/api/pic.ts
+++ pages/api/pic.ts
@@ -4,12 +4,7 @@ import { Readable } from "stream";
 
 async function Pic(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const base =
-      process.env.VERCEL_ENV === "production"
-        ? "https://" + process.env.VERCEL_URL
-        : "http://" + process.env.VERCEL_URL;
-    const url = new URL("/thumbs_up.jpeg", base).toString();
-    console.log("url", url);
+    const url = getImgUrl(req);
     await Promise.all([
       catchUpIcons().then((resp) => console.log("resp", resp)),
       fetch(url).then(async (fetchResp) => {
@@ -24,6 +19,21 @@ async function Pic(req: NextApiRequest, res: NextApiResponse) {
     console.error("e", e);
     res.status(500).end();
   }
+}
+
+// default is thumbs up
+function getImgUrl(req: NextApiRequest) {
+  const path = req.query.q;
+  if (path) {
+    console.info("external image");
+    return path as string;
+  }
+  console.info("default image");
+  const base =
+    process.env.VERCEL_ENV === "production"
+      ? "https://" + process.env.VERCEL_URL
+      : "http://" + process.env.VERCEL_URL;
+  return new URL("/thumbs_up.jpeg", base).toString();
 }
 
 export default Pic;
