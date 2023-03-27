f270fe93c4767c16410dafd068c38f1d293cd481
StephenHogsten <StephenHogsten@users.noreply.github.com>
2023-03-27 11:34:37 -0700

    tweaks

--- pages/api/pic.ts
+++ pages/api/pic.ts
@@ -1,23 +1,34 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { createReadStream, ReadStream } from "fs";
+import type { ReadStream } from "fs";
 import { catchUpIcons } from "backend/services/catchUpIcons";
 // import img from "./thumbs_up.jpeg";
 
 async function Pic(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const url = new URL("/thumbs_up.jpeg", "http://localhost:3000");
-    console.log("new url", url);
-    const resp = await catchUpIcons();
-    console.log("resp", resp);
-    // res.send(img);
-    await fetch(url).then((fetchResp) => {
-      // const x = createReadStream(fetchResp.body)
-      (fetchResp.body as unknown as ReadStream).pipe(res);
-    });
-    // req.
-    // res.redirect("/thumbs_up.jpeg");
-    // const fileStream = createReadStream('/public/thumbs_up.jpeg');
-    // fileStream.pipe(res);
+    const base =
+      process.env.VERCEL_ENV === "production"
+        ? "https://" + process.env.VERCEL_URL
+        : "http://" + process.env.VERCEL_URL;
+    const url = new URL("/thumbs_up.jpeg", base);
+    await Promise.all([
+      catchUpIcons().then((resp) => console.log("resp", resp)),
+      fetch(url).then((fetchResp) => {
+        // const x = createReadStream(fetchResp.body)
+        // this can end er
+        return new Promise((resolve, reject) => {
+          (fetchResp.body as unknown as ReadStream)
+            .pipe(res)
+            .on("close", () => {
+              console.log("success end");
+              resolve(true);
+            })
+            .on("error", (e) => {
+              console.error("error", e);
+              reject(e);
+            });
+        });
+      }),
+    ]);
   } catch (e) {
     console.error("e", e);
     res.status(500).end();
